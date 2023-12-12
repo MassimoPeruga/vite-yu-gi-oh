3276415356163f6b7fe16bf89d111ae6632f7707
MassimoPeruga <143251790+MassimoPeruga@users.noreply.github.com>
2023-12-12 18:11:22 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  name: 'DefaultTemplate',
+
+  data() {
+    return {
+      text: 'Template VueJS',
+    };
+  },
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+//file di partenza Vue
+import { createApp } from 'vue';
+import App from './App.vue';
+//file di partenza stili progetto
+import './assets/scss/style.scss';
+//file di partenza js
+import './assets/js/myscript'
+
+createApp(App).mount('#app');
